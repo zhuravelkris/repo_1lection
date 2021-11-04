7e091f0840cc00bf1f9911b23a0b512a4372339a
zhuravel <kris.zhuravel@gmail.com>
2021-11-04 22:49:59 +0000

    add Example 013 Methods

---
+++ Example012_For/Program.cs
@@ -0,0 +1,64 @@
+﻿// // Нарисовать квадраты 
+
+
+// //                      20        10
+// void DrawRectangle(int posX, int posY, int width, int height)
+// {
+//     int raw = 0;
+//     while(raw < height){
+//     Console.SetCursorPosition(posX, posY+ raw);
+//     int count = 0;
+//     while(count < width)
+//     {
+//         Console.Write("+");
+//         count++;
+//     }
+//     raw++;
+//     }
+// }
+
+// DrawRectangle(5, 3, 9, 4);
+
+
+// Нужно написать программу решения квадратных уравнений
+
+//  Есть три вида квадратных уравнений - без корней, с одним корнем, с 2 корнями
+// Чтобы выяснить, сколько корней в уравнении, нужно найти дискриминант
+// d = b^2 - 4ac
+// если d < 0 - корней нет
+// если d = 0  - один корень
+// если d > 0 - 2 различных корня
+
+// Ax^2 + Bx + c = 0
+// 1x^2 - 2x + 6 = 0
+
+(double, double) Solve(double a, double b, double c) // берем код с частным решением и с помощью метода преобразуем в общее
+{
+    double d = b * b - 4 * a * c;
+    // Console.Write("Дискриминант равен ");
+    // Console.WriteLine(d);
+
+    if (d < 0)
+    {
+        return(double.NaN, double.NaN);
+        //Console.WriteLine("Действительных корней нет");
+    }
+    else
+    {
+        double x1 = (-b + Math.Sqrt(d)) / (2 * a);
+        double x2 = (-b - Math.Sqrt(d)) / (2 * a);
+        return (x1,x2);
+
+    //     Console.WriteLine(x1);
+    //     Console.WriteLine(x2);
+
+    }
+    // Console.WriteLine();
+
+}
+
+(double, double) roots =  Solve(1, -7, 6); Console.WriteLine(roots);
+
+roots = Solve(1, 0, 6);Console.WriteLine(roots);
+roots = Solve(1,0,-4); Console.WriteLine(roots);
+roots = Solve(1,2,-4);Console.WriteLine(roots);
---
+++ Example013_Methods/Program.cs
@@ -0,0 +1,166 @@
+﻿// //Вид 1  методы, которые ничего не принимают, ничего не возвращают
+// void Method() //здесь аргументов в скобках нет
+// {
+//     Console.Writeline("Автор...");
+// }
+// // Method(); вызов метода
+
+
+// Вид 2 методы, которые что-то принимают, ничего не возвращают
+
+// void Method2(string msg)
+// {
+//     Console.WriteLine(msg);
+// }
+
+// Method21(msg: "Текст сообщения");    // вызов метода
+// или вот так
+
+// void Method21(string msg, int count)
+// {int i = 0;
+// while(i < count)
+// {
+//     Console.WriteLine(msg);
+//     i++;
+// }
+// }
+// // Method21(msg: "Текст", count: 4);
+// // или так
+// Method21(count: 4, msg: "новый текст");
+
+
+// Вид 3 Ничего не принимают, что-то возвращают
+
+// int Method3()
+
+// {
+//     return DateTime.Now.Year;
+// }
+// int year = Method3();
+// Console.WriteLine(year);
+
+
+// Вид4 Методы, которые что-то принимают и что-то возвращают
+
+// string Method4(int count, string text) //строку text будем компоновать count раз 
+// {
+//     int i = 0;
+//     string result = String.Empty;
+//     while(i < count)
+//     {
+//         result = result + text;
+//         i++;
+//     }
+//     return result;
+// }
+// string res = Method4(10, "z");
+// Console.WriteLine(res);
+
+// Оператор цикла for 
+
+// string Method4(int count, string text) //строку text будем компоновать count раз 
+// {
+//     string result = String.Empty;
+//     for (int i = 0; i < count; i++)
+//     {
+//         result = result + text;
+//     }
+//     return result;
+// }
+// string res = Method4(10, "z");
+// Console.WriteLine(res);
+
+// Для того, чтобы понять, как использовать цикл for, рассмотрим задачу вывода 
+// на экран таблицы умножения
+
+// for (int i = 2; i <= 10; i++)
+// {
+//     for (int j = 2; j <= 10; j++)
+//     {
+//         Console.WriteLine($"{i} x {j} = {i*j}");
+//     }
+//     Console.WriteLine();
+// }
+
+//НОВАЯ ЗАДАЧА
+
+//Дан текст, в котором нужно все пробелы заменить черточками,
+//маленькие буквы "к" заменить на большие "К",
+//а большие "С" заменить на маленькие "с"
+
+// string text = " - Я думаю,  - сказал князь, улыбась, - что, "
+//             + "ежели бы вас послали вместо нашего милого Винценгероде,"
+//             + "вы бы взяли приступом согласие прусского короля. "
+//             + "Вы так красноречивы. Вы дадите мне чаю?";
+
+// //string s = "qwerty"
+// //            012345
+// // s[3] // r - обратились к третьему символу строки по идентификатору (номеру символа в строке)
+
+
+// string Replace(string text, char oldValue, char newValue)
+// {
+//     string result = String.Empty;
+//     int length = text.Length; //заводим переменную равную длине текста
+//     for (int i = 0; i < length; i++)
+//     {
+//         if (text[i] == oldValue) result = result + $"{newValue}";
+//         else result = result + $"{text[i]}";
+//     }
+
+//     return result;
+// }
+// string newText = Replace(text, ' ', '|');
+// Console.WriteLine(newText);
+// Console.WriteLine();
+// newText = Replace(text, 'к', 'К');
+// Console.WriteLine(newText);
+// Console.WriteLine();
+// newText = Replace(text, 'С', 'с');
+// Console.WriteLine(newText);
+// Console.WriteLine();
+
+
+// НОВАЯ ЗАДАЧА
+// Дан массив из чисел, необходимо отсортировать элемнты массива по возрастанию 
+
+int [] arr = {1,5,4,3,2,6,7,1,1};
+
+//напишем метод, который будет выводить массив на экран, сделаем это 
+// несколькими способами
+
+void PrintArray(int[]array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+// теперь пишем метод, который будет упорядочивать массив
+
+void SelectionSort( int[] array)
+{
+    for (int i = 0; i < array.Length  - 1; i++)
+    {
+        int minPosition = i;
+        
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] < array[minPosition]) minPosition = j; 
+        }
+
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+    }
+}
+
+
+
+PrintArray(arr);
+
+SelectionSort(arr);
+PrintArray(arr);
